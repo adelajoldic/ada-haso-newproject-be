2f88205f54ef822f64911487048e1902b2e7bad9
Adela Joldic <adela.joldic@stu.ssst.edu.ba>
2023-09-06 01:53:34 +0200

    Init commit

---
+++ src/main/java/com/adahasonewprojectbe/AdaHasoNewprojectBeApplication.java
@@ -0,0 +1,13 @@
+package com.adahasonewprojectbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdaHasoNewprojectBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdaHasoNewprojectBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adahasonewprojectbe/AdaHasoNewprojectBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adahasonewprojectbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdaHasoNewprojectBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
